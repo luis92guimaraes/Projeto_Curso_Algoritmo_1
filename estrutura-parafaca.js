2d1c22ac3818422da26ecd4ce1ba1e914c07ab1d
Luis92Guimaraes <luis92guimaraes@outlook.com>
2023-05-02 17:58:26 -0300

    Primeiro commit do código

---
+++ estrutura-parafaca.js
@@ -0,0 +1,24 @@
+/*
+Var
+   numero, fatorial, contador : inteiro
+
+Inicio
+
+    escreva("Digite o número para calcular o fatorial: ")
+    leia(numero)
+    fatorial := 1
+    para contador de 1 ate numero faca
+         fatorial := fatorial * contador
+    fimpara
+    escreva("O fatorial de ", numero, " é: ", fatorial)
+
+*/
+function acaoBotao() {
+    var numero, fatorial
+    numero = prompt("Digite o número para calcular o fatorial: ")
+    fatorial = 1
+    for (var contador = 1; contador <= numero; contador++) {
+        fatorial = fatorial * contador        
+    }
+    document.getElementById("paragrafo").innerText = "O fatorial de " + numero + " é: " + fatorial
+}
